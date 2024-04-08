10864c5c82fd2c85377115f5deba605819120193
ntmaum <ntmaum@gmail.com>
2024-04-09 00:52:14 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/me.css';
+import '../styles/components/about.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+
+mobileNav();
+darkMode();
